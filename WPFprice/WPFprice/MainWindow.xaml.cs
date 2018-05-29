99fa00e56e72dc757370b79ad899ee6ac34da3e3
MaxCh330 <asdfgtrt@gmail.com>
2018-05-29 21:47:20 +0800

    no message

---
+++ WPFprice/WPFprice/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFprice
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void addbutton_Click(object sender, RoutedEventArgs e)
+        {
+            item item = new item();
+
+            toDoList.Children.Add(item);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            string data = "";
+
+            foreach (item item in toDoList.Children)
+            {
+
+                data += item.day + "|" + item.itemName + "|" + item.itemPrice + "\r\n";
+            }
+
+            System.IO.File.WriteAllText(@"D:\SaveDataTest", data);
+        }
+    }
+}
---
+++ WPFprice/WPFprice/item.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFprice
+{
+    /// <summary>
+    /// item.xaml 的互動邏輯
+    /// </summary>
+    public partial class item : UserControl
+    {
+        public item()
+        {
+            InitializeComponent();
+        }
+
+       public int itemPriceValue
+       {
+            get
+            {
+                return int.Parse(itemPrice.Text);
+            }
+            set
+            {
+                itemPrice.Text = value.ToString();
+            }
+       }
+    }
+}
